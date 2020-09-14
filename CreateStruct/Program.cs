b90c5021300864ea5893dffeecef91f6aac01541
GDSTanis <gota88gota88@gmail.com>
2020-09-14 13:12:46 +0300

    add  the code (struct)

--- CreateStruct/Program.cs
+++ CreateStruct/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CreateStruct
+namespace Praktika1
 {
     class Program
     {
+        struct Person
+        {
+            public string firstName;
+            public string lastName;
+            public int age;
+            public Person(string _firstName, string _lastName, int _age)
+            {
+                firstName = _firstName;
+                lastName = _lastName;
+                age = _age;
+            }
+            public override string ToString()
+            {
+                return firstName + " " + lastName + ", age " + age;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p = new Person("Tony", "Allen", 32);
+            Console.WriteLine(p);
         }
+
     }
 }
